386ca02a946aa986b811670e3d9c4520f187e320
midarsed <kubased2009@gmail.cz>
2026-06-13 18:08:27 +0200

    rychlost

--- zijemetu2/app/layout.tsx
+++ zijemetu2/app/layout.tsx
@@ -4,7 +4,7 @@ import { AuthProvider } from './lib/auth-context'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import { Analytics } from "@vercel/analytics/next"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata: Metadata = {
   title: 'Žijeme tu! – Strážnice',
   description: 'Sdružení nezávislých kandidátek Strážnice. Hlasujte o projektech pro naše město.',
